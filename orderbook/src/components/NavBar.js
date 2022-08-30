5f7bcefad802756dd2e02726de96d1f8cf8d330d
Ila Esmaeili <87114919+ilaesm@users.noreply.github.com>
2022-08-30 02:42:15 -0700

    Almost done

---
+++ orderbook/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {RiStockFill} from 'react-icons/ri'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div className = 'navbar'>
+    <h1><RiStockFill />Orderbook<RiStockFill /></h1>
+    </div>
+  )
+}
+
+export default NavBar
